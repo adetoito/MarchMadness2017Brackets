fd5b41be6f66849f358345cad7895870171d5d8d
vianrosal <pbgaming2225@gmail.com>
2017-03-01 13:53:10 -0500

    team class

---
+++ src/com/company/Team.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * Created by vianrosal on 3/1/17.
+ */
+public class Team implements Comparable{
+    public Team(String n,int w,int l){
+        name = n;
+        wlr = w/l;
+    }
+    public String name;
+    public double wlr;
+    public int compareTo(Object t){
+        Team tt = (Team)t;
+        if(tt.wlr > wlr){
+            return -1;
+        }
+        else if( tt.wlr== wlr){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+}
